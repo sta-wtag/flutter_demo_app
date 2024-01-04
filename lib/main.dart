97d25eecb570bab5db414204f5d6858792c463d5
“sta-wtag” <sadia.tabassum@welldev.io>
2024-01-04 14:12:05 +0600

    add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter_demo_application/screens/ProfileScreen.dart';
 import 'package:flutter_demo_application/screens/SplashScreen.dart';
 import 'package:flutter_demo_application/screens/SearchScreen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
